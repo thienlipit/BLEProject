869fdff2fa7549ddf5e68d7b60d524e9c671b7f1
thienlipit <tranthienrs24@gmail.com>
2023-01-09 17:11:22 +0700

    get RSSI of wifi

--- QRCodeScanner/app/src/main/java/com/example/qrcodescanner/MainActivity.kt
+++ QRCodeScanner/app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.qrcodescanner
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 import com.example.qrcodescanner.databinding.ActivityMainBinding
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
@@ -10,28 +20,25 @@ import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
 
-
-
-
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-//        val qrCodeGenerate = findViewById<ImageView>(R.id.qrCode)
+        val tvQRContent = binding.tvContent
+        val tvWifiInfo = binding.tvWifiInfo
 
         try {
             val barcodeEncoder = BarcodeEncoder()
-            val bitmap = barcodeEncoder.encodeBitmap("content", BarcodeFormat.QR_CODE, 400, 400)
+            val bitmap = barcodeEncoder.encodeBitmap("Android Wifi Rssi Value", BarcodeFormat.QR_CODE, 400, 400)
             binding.qrCode.setImageBitmap(bitmap)
         } catch (e: Exception) {
         }
 
-        // Register the launcher and result handler
         // Register the launcher and result handler
         val barcodeLauncher = registerForActivityResult(
             ScanContract()
@@ -39,14 +46,28 @@ class MainActivity : AppCompatActivity() {
             if (result.contents == null) {
                 Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
             } else {
-                Toast.makeText(this@MainActivity, "Scanned: " + result.contents, Toast.LENGTH_LONG)
-                    .show()
+                tvQRContent.text = result.contents
             }
         }
-
 
         binding.qrDecode.setOnClickListener {
             barcodeLauncher.launch(ScanOptions())
         }
+
+        val wifiManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        } else {
+            TODO("VERSION.SDK_INT < M")
+        }
+        val info = wifiManager.connectionInfo
+        val ssid = info.ssid
+        val rssi = info.rssi
+        tvWifiInfo.text = info.toString()
+
+        val result = wifiManager.scanResults.size
+        Log.e("listWifiInfo", result.toString())
+
+
     }
+
 }
